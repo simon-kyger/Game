0fce42c6675d577d57d8fe7de5a4796fa7b81b80
simon <simonkyger@hotmail.com>
2016-09-14 19:48:21 -0400

    finished with socket io basics

--- Game/app.js
+++ Game/app.js
@@ -9,10 +9,22 @@ app.get('/', function (req, res) {
 server.listen(port);
 
 var io = require('socket.io')(server);
+
+//upon connection
 io.sockets.on('connection', function (socket) {
+
+    //send a message to node console on server
     console.log('Someone has connected');
-    
+
+    //upon MessageToServer() triggering from client
     socket.on('MessageToServer', function (data) {
+        
+        //log that we got it here on the server
         console.log('Someone clicked the button');
+        
+        //but also send a message back to the client console
+        socket.emit('MessageFromServer', {
+            msg: 'Hello from the Server!',
+        });
     });
 });
