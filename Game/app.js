d157434a636235dc3d5ed9d960d496b0de472a94
simon <simonkyger@hotmail.com>
2016-09-14 19:24:00 -0400

    Added socket.io with a mock test dummy of an html client that sends socket data to the server.

---
+++ Game/app.js
@@ -0,0 +1,18 @@
+﻿var app = require('express')();
+var server = require('http').Server(app);
+var port = 1337;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+server.listen(port);
+
+var io = require('socket.io')(server);
+io.sockets.on('connection', function (socket) {
+    console.log('Someone has connected');
+    
+    socket.on('MessageToServer', function (data) {
+        console.log('Someone clicked the button');
+    });
+});
