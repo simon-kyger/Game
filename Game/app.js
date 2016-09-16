2d4de72ee2182b286062512534a11e02b2e065e7
simon <simonkyger@hotmail.com>
2016-09-16 06:23:24 -0400

    a new hero arrives, animated, with a forest background. lots of bugs and modularization is becoming a necessity at this point, but it looks pretty.

--- Game/app.js
+++ Game/app.js
@@ -17,25 +17,27 @@ server.listen(port);
 var PLAYER_LIST = {};
 var Player = function (id) {
     var self = {
-        x: 0,
-        y: 0,
+        x: 128,
+        y: 64,
         id: id,
         number: Math.floor(10 * Math.Random),
         dirUp: false,
         dirDown: false,
         dirLeft: false,
         dirRight: false,
-        spd: 10
+        dir: 'right',
+        spd: 0,
+        frame: 0
     }
     self.updatePosition = function() {
-        if (self.dirUp)
-            self.y -= self.spd;
-        if (self.dirDown)
-            self.y += self.spd;
-        if (self.dirLeft)
-            self.x -= self.spd;
-        if (self.dirRight)
-            self.x += self.spd;
+            if (self.dirUp)
+                self.y -= self.spd;
+            if (self.dirDown)
+                self.y += self.spd;
+            if (self.dirLeft)
+                self.x -= self.spd;
+            if (self.dirRight)
+                self.x += self.spd;
     }
     return self;
 }
@@ -52,28 +54,38 @@ io.sockets.on('connection', function (socket) {
 
     //send a message to node console on server
     console.log('Someone has connected');
-
-    //upon MessageToServer() triggering from client
-    socket.on('MessageToServer', function (data) {
-        
-        //log that we got it here on the server
-        console.log('Someone clicked the button');
-        
-        //but also send a message back to the client console
-        socket.emit('MessageFromServer', {
-            msg: 'Hello from the Server!',
-        });
-    });
     
     socket.on('keypress', function (data) {
-        if (data.inputId === 'up')
+        if (data.inputId === 'up') {
             player.dirUp = data.state;
-        if (data.inputId === 'down')
+            if (data.state)
+                player.spd = 2;
+            else
+                player.spd = 0;
+        }
+        if (data.inputId === 'down') {
             player.dirDown = data.state;
-        if (data.inputId === 'left')
+            if (data.state)
+                player.spd = 2;
+            else
+                player.spd = 0;
+        }
+        if (data.inputId === 'left') {
             player.dirLeft = data.state;
-        if (data.inputId === 'right')
+            player.dir = data.inputId;
+            if (data.state)
+                player.spd = 2;
+            else
+                player.spd = 0;
+        }
+        if (data.inputId === 'right') {
             player.dirRight = data.state;
+            player.dir = data.inputId;
+            if (data.state)
+                player.spd = 2;
+            else
+                player.spd = 0;
+        }
     });
 
     socket.on('disconnect', function () {
@@ -83,19 +95,53 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
+var map = [
+    [0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]
+];
+
+var mapoverlay = [
+    [6, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 6, 0, 3, 7, 0, 0, 0],
+    [0, 6, 5, 0, 1, 0, 0, 0, 0, 7, 6, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 2, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 6],
+    [0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 5, 0, 0, 2, 0, 0, 0],
+    [1, 0, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 3, 0, 0, 0, 4, 1, 4, 2, 0, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 6, 1, 2, 2],
+    [2, 1, 0, 1, 0, 4, 0, 0, 2, 0, 2, 3, 0, 0, 1, 1, 0, 0, 2],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 7, 1, 0, 0, 0, 0, 2, 0, 6, 0, 0, 1, 0]
+];
+
 setInterval(function () {
     var data = [];
     for (var i in PLAYER_LIST) {
         var player = PLAYER_LIST[i];
         player.updatePosition();
+        if (player.frame > 3)
+            player.frame = 0;
         data.push({
             id: player.id,
             x: player.x,
-            y: player.y
+            y: player.y,
+            dir: player.dir,
+            spd: player.spd,
+            frame: player.frame,
+            map: map,
+            mapoverlay: mapoverlay
         });
+        player.frame++;
     }
     for (var i in SOCKET_CONNECTIONS) {
         var socket = SOCKET_CONNECTIONS[i];
         socket.emit('positionalData', data);
     }
-}, 1000/30);
+}, 1000/20);
