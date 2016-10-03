bdeda9bd47f81318cb17d70899804ef0078d1e41
simon <simonkyger@hotmail.com>
2016-10-03 16:36:10 -0400

    implemented slice. implemented isalive system. implemented hp system. implemented a range on both slice and sleep. there are probably tons of bugs, but multiplayer testing now is needed after i add in some art.

--- Game/app.js
+++ Game/app.js
@@ -16,7 +16,7 @@ server.listen(port);
 const TW = 128;
 const TH = 128;
 //player Class
-var Player = function (id, name, pclass, realm) {
+var Player = function (id, name, pclass, realm, ability1, ability2, ability3, ability4, hpMAX) {
     var self = {
         x: 128,
         y: 128,
@@ -38,15 +38,23 @@ var Player = function (id, name, pclass, realm) {
         realm: realm,
         sframe: 0,
         status: {
-            slept: false,
-            snared: false,
-            stunned: false
+            slept: 0,
+            snared: 0,
+            stunned: 0
         },
         interupted: false,
-        isCasting: false
+        isCasting: false,
+        ability1: ability1,
+        ability2: ability2,
+        ability3: ability3,
+        ability4: ability4,
+        group: null,
+        hp: hpMAX,
+        hpMAX: hpMAX,
+        isAlive: true
     }
     self.updatePosition = function () {
-        if (self.status.slept == true)
+        if (self.status.slept > 0 || self.isAlive == false)
             return;
         if (self.movUp) {
             self.isCasting = false;
@@ -106,6 +114,13 @@ var Player = function (id, name, pclass, realm) {
             sframe: self.sframe,
             status: self.status,
             isCasting: self.isCasting,
+            ability1: self.ability1,
+            ability2: self.ability2,
+            ability3: self.ability3,
+            ability4: self.ability4,
+            hp: self.hp,
+            hpMAX: self.hpMAX,
+            isAlive: self.isAlive
         };
     }
     self.getUpdatePack = function () {
@@ -117,7 +132,11 @@ var Player = function (id, name, pclass, realm) {
             spd: self.spd,
             status: self.status,
             interupted: self.interupted,
-            isCasting: self.isCasting
+            isCasting: self.isCasting,
+            group: null,
+            hp: self.hp,
+            hpMAX: self.hpMAX,
+            isAlive: self.isAlive,
         };
     }
 
@@ -127,13 +146,39 @@ var Player = function (id, name, pclass, realm) {
 }
 Player.list = {};
 Player.onConnect = function (socket, name, pclass, realm) {
-    var player = new Player(socket.id, name, pclass, realm);
+    var ability1, ability2, ability3, ability4, hpMAX;
+    if (pclass == 'Mage') {
+        ability1 = 'sleep';
+        ability2 = null;
+        ability3 = null;
+        ability4 = null;
+        hpMAX = 100;
+    } else if (pclass == 'Warrior') {
+        ability1 = 'slice';
+        ability2 = null;
+        ability3 = null;
+        ability4 = null;
+        hpMAX = 250;
+    } else if (plcass == 'Priest') {
+        ability1 = null;
+        ability2 = null;
+        ability3 = null;
+        ability4 = null;
+        hpMAX = 200;
+    } else if (pclass == 'Thief') {
+        ability1 = null;
+        ability2 = null;
+        ability3 = null;
+        ability4 = null;
+        hpMAX = 150;
+    } else {
+        return; //invalid class selection
+    }
+    var player = new Player(socket.id, name, pclass, realm, ability1, ability2, ability3, ability4, hpMAX);
     for (var i in SOCKET_CONNECTIONS) {
         SOCKET_CONNECTIONS[i].emit('addToChat', player.name +' has connected.');
     }
-    for (var i in SOCKET_CONNECTIONS) {
-        SOCKET_CONNECTIONS[i].emit('addToChat', 'HELLO!');
-    }
+
     socket.on('keypress', function (data) {
         if (data.state === 'chatting')
             player.movUp = player.movDown = player.movLeft = player.movRight = false;
@@ -151,15 +196,14 @@ Player.onConnect = function (socket, name, pclass, realm) {
         }
     });
     
-    socket.on('ability', function (data) {
+    socket.on('ability1', function (data) {
         var caster;
-        var target;
-        var counter = 0;
-        var maxCounter = 2000;
+        var type;
         var target;
         for (var i in Player.list) {
             if (Player.list[i].id == data.self.id) {
                 caster = Player.list[i];
+                type = Player.list[i].ability1;
                 break;
             }
         }
@@ -169,53 +213,14 @@ Player.onConnect = function (socket, name, pclass, realm) {
                 break;
             }
         }
-        //preconditional tests against the cast are made here
-        if (caster.status.slept) {
-            SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'You cannot cast while asleep!', 'orange');
-            return;
-        }
-        
-        //begin the cast
-        for (var i in SOCKET_CONNECTIONS) {
-            SOCKET_CONNECTIONS[i].emit('addToChat', caster.name + ' begins casting a spell.', '#4c4cff');
-            caster.isCasting = true;
-        }
-        castInteruptCheck();
-        
-        //during the cast
-        function castInteruptCheck() {
-            var startCast = setInterval(function () {
-                if (caster.movUp || caster.movDown || caster.movLeft || caster.movRight || caster.interupted) {
-                    SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'Your cast has been interupted!', '#8EE5EE');
-                    caster.isCasting = false;
-                    clearInterval(startCast);
-                } else {
-                    counter += 50;
-                    if (counter >= maxCounter) {
-                        sleep(target);
-                        caster.isCasting = false;
-                        clearInterval(startCast);
-                    }
-                }
-            }, 50);
-        }
-        
-        //cast has completed, apply effect to target
-        function sleep(target) {
-            for (var i in SOCKET_CONNECTIONS) {
-                SOCKET_CONNECTIONS[i].emit('addToChat', target.name + ' has been slept.', 'orchid');
-            }  
-            target.status.slept = true;
-            console.log(target.status);
-            target.isCasting = false;
-            target.interupted = true;
-            setTimeout(function () {
-                target.status.slept = false;
-                target.interupted = false;
-                for (var i in SOCKET_CONNECTIONS) {
-                    SOCKET_CONNECTIONS[i].emit('addToChat', target.name + ' has awoken.', 'orchid');  
-                } 
-            }, 5000);
+        var xdif = caster.x - target.x;
+        var ydif = caster.y - target.y;
+        var distance = Math.sqrt((xdif * xdif) + (ydif * ydif));
+        //console.log('distance: ' +distance);
+        if (type == 'sleep') {
+            abilitySleep(target, caster, distance);
+        } else if (type == 'slice') {
+            abilitySlice(target, caster, distance);
         }
     });
 
@@ -224,6 +229,119 @@ Player.onConnect = function (socket, name, pclass, realm) {
         player: Player.getAllInitPack()
     });
 }
+function abilitySlice(target, caster, distance) {
+    var counter = 0;
+    var maxCounter = 1000;
+    if (caster.status.slept) {
+        SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'You cannot cast while asleep!', 'orange');
+        return;
+    }
+    if (distance > 150) {
+        SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'Your target is out of range', 'orange');
+        return;
+    }
+    //begin attack
+    for (var i in SOCKET_CONNECTIONS) {
+        SOCKET_CONNECTIONS[i].emit('addToChat', caster.name + ' slices ' + target.name + '.', '#4c4cff');
+        caster.isCasting = true;
+    }
+    //during the cast
+    var startCast = setInterval(function () {
+        if (distance > 150) {
+            SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'Your target is out of range!', '#8EE5EE');
+            caster.isCasting = false;
+            clearInterval(startCast);
+        } else if (caster.status.slept > 0) {
+            SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'Your attack was interupted!', '#8EE5EE');
+            caster.isCasting = false;
+            clearInterval(startCast);
+        } else {
+            counter += 50;
+            if (counter >= maxCounter) {
+                slice(target);
+                caster.isCasting = false;
+                clearInterval(startCast);
+            }
+        }
+    }, 50);
+    function slice(target) {
+        var dmg = target.hpMAX / 5;
+        SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'You sliced ' + target.name + ' for ' + dmg + ' damage!', 'orange');
+        SOCKET_CONNECTIONS[target.id].emit('addToChat', caster.name + ' attacks you for '+ dmg + ' damage!', 'red');
+        target.hp -= target.hpMAX / 5;
+        target.status.slept = 0;
+        if (target.hp < 1) {
+            target.isAlive = false;
+            for (var i in SOCKET_CONNECTIONS) {
+                SOCKET_CONNECTIONS[i].emit('addToChat', target.name + ' was just killed in undefined by '+ caster.name , 'green');
+                caster.isCasting = true;
+            }
+        }
+    }
+}
+
+function abilitySleep(target, caster, distance) {
+    var counter = 0;
+    var maxCounter = 2000;
+    //preconditional tests against the cast are made here
+    if (caster.status.slept) {
+        SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'You cannot cast while asleep!', 'orange');
+        return;
+    }
+    if (distance > 700) {
+        SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'Your target is out of range', 'orange');
+        return;
+    }
+    
+    //begin the cast
+    for (var i in SOCKET_CONNECTIONS) {
+        SOCKET_CONNECTIONS[i].emit('addToChat', caster.name + ' begins casting a spell.', '#4c4cff');
+        caster.isCasting = true;
+    }
+    
+    //during the cast
+    var startCast = setInterval(function () {
+        if (caster.movUp || caster.movDown || caster.movLeft || caster.movRight || caster.interupted) {
+            SOCKET_CONNECTIONS[caster.id].emit('addToChat', 'Your cast has been interupted!', '#8EE5EE');
+            caster.isCasting = false;
+            clearInterval(startCast);
+        } else {
+            counter += 50;
+            if (counter >= maxCounter) {
+                sleep(target);
+                caster.isCasting = false;
+                clearInterval(startCast);
+            }
+        }
+    }, 50);
+    
+    //cast has completed, apply effect to target
+    function sleep(target) {
+        //mages are naturally immune, but let the spell cast go through anyway.
+        if (target.pclass == 'Mage') {
+            SOCKET_CONNECTIONS[caster.id].emit('addToChat', target.name + ' is immune to your spell!');
+            SOCKET_CONNECTIONS[target.id].emit('addToChat', caster.name + ' tried to sleep you, but you are immune!');
+            return;
+        }
+        for (var i in SOCKET_CONNECTIONS) {
+            SOCKET_CONNECTIONS[i].emit('addToChat', target.name + ' has been slept.', 'orchid');
+        }
+        target.status.slept += 1;
+        console.log(target.status);
+        target.isCasting = false;
+        target.interupted = true;
+        setTimeout(function () {
+            target.status.slept -= 1;
+            target.interupted = false;
+            if (target.status.slept < 1) {
+                for (var i in SOCKET_CONNECTIONS) {
+                    SOCKET_CONNECTIONS[i].emit('addToChat', target.name + ' has awoken.', 'orchid');
+                }
+            }
+        }, 10000);
+    }
+}
+
 Player.getAllInitPack = function () {
     var players = [];
     for (var i in Player.list)
@@ -244,6 +362,7 @@ Player.onDisconnect = function (socket) {
     }
 }
 Player.update = function () {
+    //this should be modified based upon distance for performance reasons.
     var data = [];
     for (var i in Player.list) {
         var player = Player.list[i];
@@ -266,18 +385,14 @@ io.sockets.on('connection', function (socket) {
         var pclass = data.pclass.toString().replace(/\r?\n$/, '');
         var realm = data.realm.toString().replace(/\r?\n$/, '');
         var username = data.username.toString().replace(/\r?\n$/, '');
-        //if (fdata != 'Warrior' || fdata != 'Mage'){
-        //    socket.emit('loginresponse', { success: false });
-        //    console.log('fdata is: ' +fdata);
-        //    return;
-        //}
+        // more validation needs to be done here
         if (data.username && data.password) {
             Player.onConnect(socket, username, pclass, realm);
             socket.emit('loginresponse', { success: true });
         } else
             socket.emit('loginresponse', { success: false });
     });   
-    //send a message to node console on server and to the clients
+
     socket.on('disconnect', function () {
         Player.onDisconnect(socket);
         delete SOCKET_CONNECTIONS[socket.id];
